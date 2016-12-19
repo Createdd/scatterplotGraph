83c513b1271a6c96bc55e8ba7951940f4c0d7596
Daniel Deutsch <deudan1010@gmail.com>
2016-12-19 23:58:35 +0100

    initial

---
+++ components/HeaderC.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class Header extends React.Component {
+  render () {
+    return (
+      <div className='container teal-text center'>
+        <h2>Visualize GDP With A Bar Chart</h2>
+      </div>
+    )
+  }
+}
